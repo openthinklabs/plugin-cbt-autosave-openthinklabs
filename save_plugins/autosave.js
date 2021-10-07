55d8b1958afe47f12a064f4b3e996bfc82eb9ae7
Wildan Maulana <wildan.m@gmail.com>
2021-10-07 22:43:43 +0700

    fix autosave plugins

--- save_plugins/autosave.js
+++ save_plugins/autosave.js
@@ -1,97 +1,93 @@
-define(["taoTests/runner/plugin"], function (pluginFactory) {
-  "use strict";
+import pluginFactory from "taoTests/runner/plugin";
 
-  return pluginFactory({
-    name: "autoSave",
+/**
+ * Returns the configured plugin
+ */
+export default pluginFactory({
+  name: "autoSave",
 
-    init: function init() {
-      var self = this;
-      var areaBroker = this.getAreaBroker();
+  init: function init() {
+    var self = this;
+    var areaBroker = this.getAreaBroker();
 
-      this.button = areaBroker.getToolbox().createEntry({
-        control: "autoSave",
-        text: "Auto Save",
-        title: "Auto Save",
-        icon: "clock",
-      });
+    this.button = areaBroker.getToolbox().createEntry({
+      control: "autoSave",
+      text: "Auto Save",
+      title: "Auto Save",
+      icon: "clock",
+    });
 
-      //this.getTestRunner().on('loaditem', function(){
-      this.getTestRunner()
-        .on("enabletools loaditem renderitem", function () {
-          if (document.querySelector(".qti-extendedTextInteraction")) {
-            var testContext = self.getTestRunner().getTestContext();
-            var testPartId = testContext.testPartId;
-            var sectionId = testContext.sectionId;
-            var itemIdentifier = testContext.itemIdentifier;
+    //this.getTestRunner().on('loaditem', function(){
+    this.getTestRunner()
+      .on("enabletools loaditem renderitem", function () {
+        if (document.querySelector(".qti-extendedTextInteraction")) {
+          var testContext = self.getTestRunner().getTestContext();
+          var testPartId = testContext.testPartId;
+          var sectionId = testContext.sectionId;
+          var itemIdentifier = testContext.itemIdentifier;
 
-            var testMap = self.getTestRunner().getTestMap();
-            var categories =
-              testMap.parts[testPartId].sections[sectionId].items[
-                itemIdentifier
-              ].categories;
+          var testMap = self.getTestRunner().getTestMap();
+          var categories =
+            testMap.parts[testPartId].sections[sectionId].items[itemIdentifier]
+              .categories;
 
-            window.endTime = new Date().getTime() + 300000;
+          window.endTime = new Date().getTime() + 300000;
 
-            //if(categories.includes('autoSave')){
-            self.show();
+          //if(categories.includes('autoSave')){
+          self.show();
 
-            window.timer = setInterval(function () {
-              var now = new Date().getTime();
-              var distance = window.endTime - now;
+          window.timer = setInterval(function () {
+            var now = new Date().getTime();
+            var distance = window.endTime - now;
 
-              var minutes = Math.floor(
-                (distance % (1000 * 60 * 60)) / (1000 * 60)
-              );
-              var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            var minutes = Math.floor(
+              (distance % (1000 * 60 * 60)) / (1000 * 60)
+            );
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+            document.querySelector('li[data-control="autoSave"]').innerHTML =
+              "Autosave in " + minutes + "m " + seconds + "s";
+
+            if (distance == 0) {
               document.querySelector('li[data-control="autoSave"]').innerHTML =
-                "Autosave in " + minutes + "m " + seconds + "s";
+                "Autosaving..";
+              window.clearInterval(window.timer);
+              self
+                .getTestRunner()
+                .jump(self.getTestRunner().getTestContext()["itemPosition"]);
+            }
+          });
+        } else {
+          self.hide();
+        }
+      })
+      .on("unloaditem unrenderitem move skip next previous jump", function () {
+        window.clearInterval(window.timer);
+        self.disable();
+      });
+  },
 
-              if (distance == 0) {
-                document.querySelector(
-                  'li[data-control="autoSave"]'
-                ).innerHTML = "Autosaving..";
-                window.clearInterval(window.timer);
-                self
-                  .getTestRunner()
-                  .jump(self.getTestRunner().getTestContext()["itemPosition"]);
-              }
-            });
-          } else {
-            self.hide();
-          }
-        })
-        .on(
-          "unloaditem unrenderitem move skip next previous jump",
-          function () {
-            window.clearInterval(window.timer);
-            self.disable();
-          }
-        );
-    },
+  render: function render() {},
 
-    render: function render() {},
+  destroy: function destroy() {
+    if (this.button && this.button.length) {
+      this.button.off("click");
+    }
+  },
 
-    destroy: function destroy() {
-      if (this.button && this.button.length) {
-        this.button.off("click");
-      }
-    },
+  enable: function enable() {
+    this.button.enable();
+  },
 
-    enable: function enable() {
-      this.button.enable();
-    },
+  disable: function disable() {
+    this.button.disable();
+  },
 
-    disable: function disable() {
-      this.button.disable();
-    },
+  show: function show() {
+    this.button.show();
+  },
 
-    show: function show() {
-      this.button.show();
-    },
-
-    hide: function hide() {
-      this.button.hide();
-    },
-  });
+  hide: function hide() {
+    this.button.hide();
+  },
 });
--- save_plugins/save_button.js
+++ save_plugins/save_button.js
@@ -1,74 +1,75 @@
-define(["taoTests/runner/plugin"], function (pluginFactory) {
-  "use strict";
+import pluginFactory from "taoTests/runner/plugin";
 
-  return pluginFactory({
-    name: "saveButton",
+/**
+ * Returns the configured plugin
+ */
+export default pluginFactory({
+  name: "saveButton",
 
-    init: function init() {
-      var self = this;
-      var areaBroker = this.getAreaBroker();
+  init: function init() {
+    var self = this;
+    var areaBroker = this.getAreaBroker();
 
-      this.button = areaBroker.getToolbox().createEntry({
-        control: "saveQuestion",
-        text: "Save Question",
-        title: "Save Question",
-        icon: "Save",
-      });
+    this.button = areaBroker.getToolbox().createEntry({
+      control: "saveQuestion",
+      text: "Save Question",
+      title: "Save Question",
+      icon: "Save",
+    });
 
-      this.button.on("click", function (e) {
-        e.preventDefault();
-        let testData = self.getTestRunner().getTestContext();
-        self.getTestRunner().jump(testData["itemPosition"]);
-      });
+    this.button.on("click", function (e) {
+      e.preventDefault();
+      let testData = self.getTestRunner().getTestContext();
+      self.getTestRunner().jump(testData["itemPosition"]);
+    });
 
-      this.getTestRunner()
-        .on("enabletools renderitem", function () {
-          var testContext = self.getTestRunner().getTestContext();
-          var testPartId = testContext.testPartId;
-          var sectionId = testContext.sectionId;
-          var itemIdentifier = testContext.itemIdentifier;
+    this.getTestRunner()
+      .on("enabletools renderitem", function () {
+        var testContext = self.getTestRunner().getTestContext();
+        var testPartId = testContext.testPartId;
+        var sectionId = testContext.sectionId;
+        var itemIdentifier = testContext.itemIdentifier;
 
-          var testMap = self.getTestRunner().getTestMap();
-          var categories =
-            testMap.parts[testPartId].sections[sectionId].items[itemIdentifier]
-              .categories;
+        var testMap = self.getTestRunner().getTestMap();
+        var categories =
+          testMap.parts[testPartId].sections[sectionId].items[itemIdentifier]
+            .categories;
 
-          if (document.querySelector(".qti-extendedTextInteraction")) {
-            self.show();
-            self.enable();
-          } else {
-            self.disable();
-            self.hide();
-          }
-        })
-        .on("disable tools unloaditem", function () {
+        if (document.querySelector(".qti-extendedTextInteraction")) {
+          self.show();
+          self.enable();
+        } else {
           self.disable();
           self.hide();
-        });
-    },
+        }
+      })
+      .on("disable tools unloaditem", function () {
+        self.disable();
+        self.hide();
+      });
+  },
 
-    render: function render() {},
+  render: function render() {},
 
-    destroy: function destroy() {
-      if (this.button && this.button.length) {
-        this.button.off("click");
-      }
-    },
+  destroy: function destroy() {
+    if (this.button && this.button.length) {
+      this.button.off("click");
+    }
+  },
 
-    enable: function enable() {
-      this.button.enable();
-    },
+  enable: function enable() {
+    this.button.enable();
+  },
 
-    disable: function disable() {
-      this.button.disable();
-    },
+  disable: function disable() {
+    this.button.disable();
+  },
 
-    show: function show() {
-      this.button.show();
-    },
+  show: function show() {
+    this.button.show();
+  },
 
-    hide: function hide() {
-      this.button.hide();
-    },
-  });
+  hide: function hide() {
+    this.button.hide();
+  },
 });
